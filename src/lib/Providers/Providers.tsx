b87fe0bde26ecebd0e7f1d347aecc0aed9b222b8
pantho0 <panthomashreky6@gmail.com>
2025-07-08 17:26:08 +0600

    redux persistor added for keep cart item on refresh

--- src/lib/Providers/Providers.tsx
+++ src/lib/Providers/Providers.tsx
@@ -2,7 +2,15 @@
 import { store } from "@/redux/store";
 import { ReactNode } from "react";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "@/redux/store";
 
 export const Providers = ({ children }: { children: ReactNode }) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 };
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,37 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./features/cartSlice";
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from "redux-persist";
+
+const persistConfig = {
+  key: "cart",
+  storage,
+};
+
+const persistedCartReducer = persistReducer(persistConfig, cartReducer);
 
 export const store = configureStore({
   reducer: {
-    cart: cartReducer,
+    cart: persistedCartReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          "FLUSH",
+          "REHYDRATE",
+          "PAUSE",
+          "PERSIST",
+          "PURGE",
+          "REGISTER",
+        ],
+      },
+    }).concat(),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
