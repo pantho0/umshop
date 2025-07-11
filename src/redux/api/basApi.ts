4d4c437df70fc2b2772c180d8edfd2adf66bcbeb
pantho0 <panthomashreky6@gmail.com>
2025-07-11 22:25:19 +0600

    accesstoken generate with refresh token functions added with rtk custom base query

--- src/redux/api/basApi.ts
+++ src/redux/api/basApi.ts
@@ -1,6 +1,15 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import {
+  BaseQueryApi,
+  BaseQueryFn,
+  createApi,
+  DefinitionType,
+  FetchArgs,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/query/react";
 
 import { RootState } from "../store";
+
+import { logOut, setUser } from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: `${process.env.NEXT_PUBLIC_API_URL}`,
@@ -14,8 +23,35 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
+const baseQueryWithRefreshToken: BaseQueryFn<
+  FetchArgs,
+  BaseQueryApi,
+  DefinitionType
+> = async (args, api, extraOptions): Promise<any> => {
+  let result = await baseQuery(args, api, extraOptions);
+  if (result?.error?.status === 401) {
+    console.log("sendiing token");
+    const res = await fetch(
+      "http://localhost:5000/api/v1/auth/generate-accesstoken",
+      {
+        method: "POST",
+        credentials: "include",
+      }
+    );
+    const data = await res.json();
+    if (data?.data?.accessToken) {
+      const user = (api.getState() as RootState).persisted.auth.user;
+      api.dispatch(setUser({ user, token: data?.data?.accessToken }));
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logOut());
+    }
+  }
+  return result;
+};
+
 export const baseApi = createApi({
   reducerPath: "baseApi",
-  baseQuery: baseQuery,
+  baseQuery: baseQueryWithRefreshToken,
   endpoints: () => ({}),
 });
