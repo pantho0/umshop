4443de32519db90165913d11a6cd9f8e5338ae48
pantho0 <panthomashreky6@gmail.com>
2025-07-22 22:48:08 +0600

    added authorization headers from nexios

--- src/app/config/nexios.config.ts
+++ src/app/config/nexios.config.ts
@@ -1,4 +1,5 @@
 import { Nexios } from "nexios-http";
+import { cookies } from "next/headers";
 
 const nexiosInstance = new Nexios({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -8,6 +9,17 @@ const nexiosInstance = new Nexios({
   },
   credentials: "include",
   timeout: 10000,
+});
+
+nexiosInstance.interceptors.request.use(async (config) => {
+  const accessToken = (await cookies()).get("accessToken")?.value;
+  if (accessToken) {
+    config.headers = {
+      ...config.headers,
+      Authorization: accessToken,
+    };
+  }
+  return config;
 });
 
 export default nexiosInstance;
