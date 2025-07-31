65ae1ed6c259c10ccaaad39e07f61228778473ab
pantho0 <panthomashreky6@gmail.com>
2025-07-31 21:35:49 +0600

    updated payment-success page loading and error design

--- src/app/(commonLayout)/payment-success/page.tsx
+++ src/app/(commonLayout)/payment-success/page.tsx
@@ -65,17 +65,47 @@ const PaymentSuccessContent: React.FC = () => {
   }, [dispatch, searchParams]);
 
   if (loading) {
-    return <div className="text-center p-8">Finalizing your order...</div>;
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="flex flex-col items-center space-y-4 p-8">
+          <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-purple-500"></div>
+          <p className="text-lg text-gray-700 font-semibold">
+            Finalizing your order...
+          </p>
+        </div>
+      </div>
+    );
   }
 
   if (error) {
     return (
-      <div className="text-center p-8 text-red-500">
-        <h1>Error</h1>
-        <p>{error}</p>
-        <Link href="/products">
-          <Button>Go to Products</Button>
-        </Link>
+      <div className="font-inter antialiased min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="container mx-auto px-4 py-8 md:py-12 max-w-md ">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="h-16 w-16 text-red-500 mx-auto mb-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
+            />
+          </svg>
+
+          <h1 className="text-2xl md:text-3xl font-bold text-gray-900 mb-4">
+            Oops! Something went wrong.
+          </h1>
+          <p className="text-gray-600 text-lg mb-8">{error}</p>
+          <Link href="/products">
+            <Button className="w-full bg-purple-600 cursor-pointer text-white font-semibold py-3 rounded-md hover:bg-purple-700 transition-colors duration-200 shadow-md">
+              Go to Products
+            </Button>
+          </Link>
+        </div>
       </div>
     );
   }
